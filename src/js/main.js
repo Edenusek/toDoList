6f3379cda359d17d45c40c5d7f6253a876668eca
Edenus <edenus27@gmail.com>
2023-05-29 20:59:14 +0200

    make added, remove and edit btn and functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,85 @@
 const content = document.querySelector("#content");
 const date = document.querySelector("#date");
-const btn = document.querySelector(".btn");
+const btn = document.querySelector(".add-btn");
 const deleteBtn = document.querySelector(".delete-btn");
-// const addedContent = document.querySelector(".added-content");
-// const addedDate = document.querySelector(".added-date");
-// const addedAray = document.querySelector(".added-box");
-const addedSection = document.querySelector(".added")
+const editBtn = document.querySelector(".edit-btn");
+const addedSection = document.querySelector(".added");
 const errorText = document.querySelector(".error");
-
-const aray = [];
+const deleteAllBtn = document.querySelector(".delete-all");
+const deletedAllCheckedBtn = document.querySelector(".delete-checked");
+const checkBoxes = document.querySelectorAll(".check-box");
 
 const handleAdd = () => {
-    const enteredDate = new Date(date.value);
-    const year = enteredDate.getFullYear();
-    const month = enteredDate.getMonth() + 1;
-    const day = enteredDate.getDate();
-
-  if (content.vaule === "" || date.value === "") {
+  if (content.value === "") {
     errorText.textContent = "Musisz uzupełnić wszystkie pola !";
     return;
   }
 
-const addedBox = document.createElement("div")
-addedBox.classList.add("added-box")
+  const addedBox = document.createElement("div");
+  addedBox.classList.add("added-box");
 
-const addedContent = document.createElement("span")
-addedContent.classList.add("added-content")
-addedContent.textContent = content.value;
+  const addedContent = document.createElement("span");
+  addedContent.classList.add("added-content");
+  addedContent.textContent = content.value;
 
-const addedDate = document.createElement("span")
-addedDate.classList.add("added-date")
-addedDate.textContent = `${day}-${month}-${year}`;
+  const checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+  checkBox.classList.add("check-box");
 
-const checkBox = document.createElement("input")
-checkBox.type = "checkbox"
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete-btn", "btn");
+  deleteButton.textContent = "usuń";
 
-const deleteButton = document.createElement("button")
-deleteButton.classList.add("delete-btn")
-deleteButton.textContent = "usuń"
+  const editButton = document.createElement("button");
+  editButton.classList.add("edit-btn", "btn");
+  editButton.textContent = "edytuj";
 
-addedBox.appendChild(addedContent)
-addedBox.appendChild(addedDate)
-addedBox.appendChild(checkBox)
-addedBox.appendChild(deleteButton)
+  addedBox.appendChild(addedContent);
+  addedBox.appendChild(checkBox);
+  addedBox.appendChild(deleteButton);
+  addedBox.appendChild(editButton);
 
-addedSection.appendChild(addedBox)
+  addedSection.appendChild(addedBox);
+
+  content.value = "";
 };
 
+const deleteHadnle = e => {
+  const addedBox = e.target.closest(".added-box");
+  addedBox.remove();
+};
 
-const deleteHadnle = (e) => {
-  const addedBox =  e.target.closest("added-box");
-  addedBox.remove()
+const editHandler = e => {
+  const addedBox = e.target.closest(".added-box");
+  const addedContent = addedBox.querySelector(".added-content");
+  const editContent = prompt("Wpisz nowe zadanie:");
+  if (editContent.trim() !== "") {
+    addedContent.textContent = editContent;
+  }
+};
+
+const deletedAllCheckedHandle = () => {
+  // for (let i =0; i < checkBoxes.length; i++){
+  //   if(checkBoxes[i].checked === true){
+  //     console.log("zaznaczony")
+  //   }
+  // }
+  console.log("działam");
+};
+
+const deleteAllHandle = () => {
+  addedSection.remove();
 };
 
 btn.addEventListener("click", handleAdd);
-// deleteBtn.addEventListener("click", deleteHadnle);
+
 addedSection.addEventListener("click", function (e) {
-    if(e.target.classList.contains("deletebtn")){
-        deleteHadnle
-    }
-})
+  if (e.target.classList.contains("delete-btn")) {
+    deleteHadnle(e);
+  } else if (e.target.classList.contains("edit-btn")) {
+    editHandler(e);
+  }
+});
+
+deleteAllBtn.addEventListener("click", deleteAllHandle);
+deletedAllCheckedBtn.addEventListener("click", deletedAllCheckedHandle);
